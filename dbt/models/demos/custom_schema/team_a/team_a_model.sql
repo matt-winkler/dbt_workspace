a4f6b20fb00bc098802d6fe51dac1a43020d3fbe
Matt Winkler <matt.winkler@fishtownanalytics.com>
2022-05-09 19:55:58 +0000

    add tagging

--- dbt/models/demos/custom_schema/team_a/team_a_model.sql
+++ dbt/models/demos/custom_schema/team_a/team_a_model.sql
@@ -1,3 +1,9 @@
 -- When you reference this in another model, notice how the schema location is changed from the project default.
 
+{{
+    config(
+        tags=['custom_schema_demo']
+    )
+}}
+
 select 'Hello, world!' as my_column
--- dbt/models/demos/custom_schema/team_b/team_b_model.sql
+++ dbt/models/demos/custom_schema/team_b/team_b_model.sql
@@ -1,2 +1,9 @@
 
+{{
+    config(
+        tags=['custom_schema_demo']
+    )
+}}
+
+
 select * from {{ ref('team_a_model') }}
