d084f409d9a1644acbd5fb1898dd277834f6d7e6
Matt Winkler <matt.winkler@fishtownanalytics.com>
2022-05-13 00:16:07 +0000

    remove extra unique key stuff

--- dbt/macros/materializations/view_sync_incremental/view_sync_get_merge_sql.sql
+++ dbt/macros/materializations/view_sync_incremental/view_sync_get_merge_sql.sql
@@ -42,23 +42,6 @@
     {%- set update_columns = config.get('merge_update_columns', default = dest_columns | map(attribute="quoted") | list) -%}
     {%- set sql_header = config.get('sql_header', none) -%}
 
-    {% if unique_key %}
-        {% if unique_key is sequence and unique_key is not mapping and unique_key is not string %}
-            {% for key in unique_key %}
-                {% set this_key_match %}
-                    DBT_INTERNAL_SOURCE.{{ key }} = DBT_INTERNAL_DEST.{{ key }}
-                {% endset %}
-                {% do predicates.append(this_key_match) %}
-            {% endfor %}
-        {% else %}
-            {% set unique_key_match %}
-                DBT_INTERNAL_SOURCE.{{ unique_key }} = DBT_INTERNAL_DEST.{{ unique_key }}
-            {% endset %}
-            {% do predicates.append(unique_key_match) %}
-        {% endif %}
-    {% else %}
-        {% do predicates.append('FALSE') %}
-    {% endif %}
 
     {{ sql_header if sql_header is not none }}
     
