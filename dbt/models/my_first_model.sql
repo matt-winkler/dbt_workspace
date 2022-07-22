960244f1665479b6a6d55520de6f1945092afe5e
Matt Winkler <matt.winkler@fishtownanalytics.com>
2022-07-22 14:53:47 +0000

    fixed pipeline error

--- dbt/models/my_first_model.sql
+++ dbt/models/my_first_model.sql
@@ -1,4 +1,3 @@
 {{config(materialized='table')}}
 
-select 'hello, world!' as col union all
-select 'hello, back' as col 
+select 'hello, world!' as col
--- dbt/models/my_second_model.sql
+++ dbt/models/my_second_model.sql
@@ -1,2 +1,4 @@
-select col1, 1 as static from {{ref('my_first_model')}}
+--select incorrect_name, 1 as static from {{ref('my_first_model')}}
+
+select col, 1 as static from {{ref('my_first_model')}}
 
