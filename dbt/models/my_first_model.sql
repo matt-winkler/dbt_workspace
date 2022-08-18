b141fd710044acd27cc314602f112ce0e158cc9a
Matt Winkler <matt.winkler@fishtownanalytics.com>
2022-08-18 18:08:19 +0000

    add test to my_first_model

--- dbt/models/my_first_model.sql
+++ dbt/models/my_first_model.sql
@@ -1,8 +1,6 @@
 {{
     config(
-        materialized='table', 
-        tags=['classification=sensitive'],
-        post_hook="{{dbt_tag_as_snowflake_object_tag(model)}}"
+        materialized='table'
     )
 }}
 
